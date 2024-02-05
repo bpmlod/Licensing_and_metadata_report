2d4c9a49c6c7af4bf9698856cd3cbfde7282c288
Victor Rodriguez Doncel <vrodriguez@fi.upm.es>
2024-02-05 11:45:24 +0100

    Testing whether a first import looks good

---
+++ Guidelines/config.js
@@ -0,0 +1,22 @@
+var respecConfig = {
+    specStatus: "CG-DRAFT",
+    shortName: "odrl-lr",
+    editors: [	{   name:       "Penny Labropoulou",
+         	company:    "ILSP, Athena Research Center", 
+                   companyURL: "https://www.ilsp.gr/en"            
+         },
+         	 {  name:       "Víctor Rodríguez-Doncel",
+                    url:        "http://cosasbuenas.es",
+                    company:    "Universidad Politécnica de Madrid",
+                   companyURL: "http://www.upm.es"
+           }
+    ],
+    group: "odrl",
+    wgPublicList: "public-odrl",
+    inlineCSS: true,
+    noIDLIn: true,
+    noLegacyStyle: false,
+    noRecTrack: true,
+    lint: true,
+    github: { repoURL: "https://github.com/w3c/odrl/", branch: "master"}
+};
